56ba5668ed3d4700f0d6b31870e42a77e9ad5589
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-05-18 16:18:59 +0200

    generar imagen random

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-function search () {
-    
+function search() {
+
     fetch('https://dog.ceo/api/breeds/image/random')
-    .then(res=>res.json())
-    .then(json=>{
-        console.log(json)
-    });
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            let divImgen = document.getElementById("divImg")
+            const generarLink = json.message;
+            divImgen.innerHTML = `<img src='${generarLink}'/>` 
+
+            //document.getElementsByClassName("picture").src = generarLink;
+
+            //nodoImg.src=`${generarLink}`
+
+            .then(json=>{
+                document.getElementById('random').src=json.message;
+                return json
+            })
+        
+
+            console.log(generarLink);
+            return json;
+        });
 }
 
 search();
+
 
 
 //en el boton random guardar las urls generadas para ver si se repite y desecharla.
