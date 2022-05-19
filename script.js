9c09252b8d5829d9d985116994c274e57b4cab86
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-05-19 16:46:41 +0200

    cambios

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let count = 0
+let linkArray = [];
+let breeds = [];
+let repeatedBreeds = [];
+
 function search() {
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
@@ -6,34 +11,82 @@ function search() {
             // let divImgen = document.getElementById("divImg")
             // const generarLink = json.message;
             // divImgen.innerHTML = `<img src='${generarLink}'/>`
-
-
             document.getElementById('picture').src = json.message;
-            return json
+            console.log(linkArray);
+            count++
+
+            const splitMessage = json.message.split('/');
+            const getBreedPosition = splitMessage[4];
+            breeds.push(getBreedPosition);
+            console.log(breeds);
+
+            if (count == 1) {
+                document.getElementById("1").src = json.message;
+            } else if (count == 2) {
+                document.getElementById("2").src = json.message;
+            } else if (count == 3) {
+                document.getElementById("3").src = json.message;
+            } else if (count == 4) {
+                document.getElementById("4").src = json.message;
+            } else if (count == 5) {
+                document.getElementById("5").src = json.message;
+            } else if (count == 6) {
+                document.getElementById("6").src = json.message;
+            } else if (count == 7) {
+                document.getElementById("7").src = json.message;
+            } else if (count == 8) {
+                document.getElementById("8").src = json.message;
+            } else if (count == 9) {
+                document.getElementById("9").src = json.message;
+            } else if (count == 10) {
+                document.getElementById("10").src = json.message;
+            } else if (count == 11) {
+                document.getElementById("11").src = json.message;
+            } else if (count == 12) {
+                document.getElementById("12").src = json.message;
+            } else if (count == 13) {
+                document.getElementById("13").src = json.message;
+            } else if (count == 14) {
+                document.getElementById("14").src = json.message;
+            } else if (count == 15) {
+                document.getElementById("15").src = json.message;
+            } else if (count == 16) {
+                document.getElementById("16").src = json.message;
+            } else if (count == 17) {
+                document.getElementById("17").src = json.message;
+            } else if (count == 18) {
+                document.getElementById("18").src = json.message;
+            } else if (count == 19) {
+                document.getElementById("19").src = json.message;
+            } else if (count == 20) {
+                document.getElementById("20").src = json.message;
+            }
+
+            linkArray.push(document.getElementById('picture').src = json.message);
+
+            localStorage.setItem("Razas", JSON.stringify(linkArray));
+            for (let i = 0; i < linkArray.length; i++) {
+                const splitLink = linkArray[i].split('/');
+                const getBreed = splitLink[4];
+                //console.log(getBreed);
+            }
+
+
         })
 
-        .then(json => {
-            let linkArray = [];
-            let count = 0
-            const button = document.getElementById('random');
-            button.onclick = function () {
-                count++
-                console.log(count);
-                linkArray.push(document.getElementById('picture').src = json.message);
-                console.log(linkArray);
+    // .then(json => {
 
-            }
-            if (count === 1) {
-                document.getElementById("uno").src = json.message;
-            }
 
-            //if()
-            //document.getElementById("uno").src = json.message;
-            return json
-        })
+    //     document.getElementById("uno").src = json.message;
+
+
+    //     //if()
+    //     //document.getElementById("uno").src = json.message;
+    //     return json
+    // })
 }
 
-search();
+//search();
 
 
 
