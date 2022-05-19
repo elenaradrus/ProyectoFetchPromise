029556924ed0cc6c8929dca75402f96df3b1f79d
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-05-19 14:22:10 +0200

    cambios

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 function search() {
-
     fetch('https://dog.ceo/api/breeds/image/random')
         .then(res => res.json())
         .then(json => {
-            console.log(json)
-            let divImgen = document.getElementById("divImg")
-            const generarLink = json.message;
-            divImgen.innerHTML = `<img src='${generarLink}'/>` 
+            // console.log(json)
+            // let divImgen = document.getElementById("divImg")
+            // const generarLink = json.message;
+            // divImgen.innerHTML = `<img src='${generarLink}'/>`
 
-            //document.getElementsByClassName("picture").src = generarLink;
 
-            //nodoImg.src=`${generarLink}`
+            document.getElementById('picture').src = json.message;
+            return json
+        })
 
-            .then(json=>{
-                document.getElementById('random').src=json.message;
-                return json
-            })
-        
+        .then(json => {
+            let linkArray = [];
+            let count = 0
+            const button = document.getElementById('random');
+            button.onclick = function () {
+                count++
+                console.log(count);
+                linkArray.push(document.getElementById('picture').src = json.message);
+                console.log(linkArray);
 
-            console.log(generarLink);
-            return json;
-        });
+            }
+            if (count === 1) {
+                document.getElementById("uno").src = json.message;
+            }
+
+            //if()
+            //document.getElementById("uno").src = json.message;
+            return json
+        })
 }
 
 search();
