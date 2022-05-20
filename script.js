1edb4c5e2b7d5dc493821795f0301c6cb399409c
Elena Radu <lena@MacBook-Air-de-Elena.local>
2022-05-20 12:46:43 +0200

    grafica hecha

--- script.js
+++ script.js
@@ -22,12 +22,53 @@ function search() {
                 const text = document.createTextNode('La imagen no se insertará al ser una raza repetida');
                 phrase.appendChild(text);
                 document.body.appendChild(phrase);
-                setTimeout (() => {
+                setTimeout(() => {
                     phrase.style.display = 'none';
                 }, 4000);
-            } 
+            }
 
             console.log(breeds);
             localStorage.setItem("Razas Repetidas", JSON.stringify(breeds));
+        })
+        .then(json => {
+            if (count == 21) {
+                const getChart = document.getElementById('myChart');
+
+                const dogChart = new Chart(getChart, {
+                    type: 'line',
+                    data: {
+                        labels: breeds,
+                        datasets: [{
+                            label: 'Razas de perros',
+                            data: [5, 2, 1, 4, 10, 6, 1, 3, 10, 20, 15, 3, 6, 8, 9, 2, 8, 13, 12, 5],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            }
+
         });
 }
