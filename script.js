6958fbd9deb1dd0ff5c555eb65014f9963cee625
Elena Radu <lena@Air-de-Elena.home>
2022-05-23 20:08:11 +0200

    terminado

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 let count = 1;
 let breeds = [];
-
+let retrieveData = [];
 function search() {
+    console.log(localStorage.length);
+    retrieveData = localStorage.getItem("Razas_de_Perros");
     fetch("https://dog.ceo/api/breeds/image/random")
         .then(res => res.json())
         .then(json => {
-
             const dogPics = document.getElementById("picture").src = json.message;
             const splitMessage = dogPics.split('/')[4];
             let repeatedBreed = breeds.includes(splitMessage);
-
+            retrieveData = JSON.parse(localStorage.getItem("Razas_de_Perros"));
+            if (retrieveData == null) {
+                retrieveData = [];
+                retrieveData.push([splitMessage, 1, json.message]);
+                localStorage.setItem("Razas_de_Perros", JSON.stringify(retrieveData));
+            } else {
+                //[[Nombre, contador], [Nombre, contador], [Nombre, contador], [Nombre, contador]];
+                var encontrado = false;
+                let i = 0;
+                while (i < retrieveData.length && !encontrado) {
+                    encontrado = encontrado || splitMessage == retrieveData[i][0];
+                    i++;
+                }
+                if (encontrado) {
+                    retrieveData[i][1] = retrieveData[i][1] + 1;
+                    retrieveData.splice(i, 1, retrieveData[i])
+                    localStorage.setItem("Razas_de_Perros", JSON.stringify(retrieveData));
+                } else {
+                    retrieveData.push([splitMessage, 1, json.message]);
+                    localStorage.setItem("Razas_de_Perros", JSON.stringify(retrieveData))
+                }
+            }
+            console.log(retrieveData);
+            retrieveData = JSON.parse(localStorage.getItem("Razas_de_Perros"));
             if (!repeatedBreed) {
                 document.getElementById(`${count}`).src = dogPics;
-                breeds.push(splitMessage);
+                breeds.push([splitMessage, 1]);
                 count++
             } else {
+                breeds.push(splitMessage);
                 const phrase = document.createElement('h3');
                 const text = document.createTextNode('La imagen no se insertará al ser una raza repetida');
                 phrase.appendChild(text);
@@ -26,20 +51,28 @@ function search() {
                     phrase.style.display = 'none';
                 }, 2000);
             }
-
-            console.log(breeds);
-            localStorage.setItem("Razas de Perros", JSON.stringify(breeds));
+            //console.log(breeds);
         })
         .then(json => {
-            if (count == 21) {
+            let nameDogs = []
+            let countDogs= []
+            retrieveData = JSON.parse(localStorage.getItem("Razas_de_Perros"));
+            for(i = 0; i < retrieveData.length;i++){
+                nameDogs.push(retrieveData[i][0])
+                
+                countDogs.push(retrieveData[i][1])
+            }
+            console.log(nameDogs)
+           
+            if (count > 20) {
                 const getChart = document.getElementById('myChart');
                 const dogChart = new Chart(getChart, {
                     type: 'bar',
                     data: {
-                        labels: breeds,
+                        labels: nameDogs,
                         datasets: [{
-                            label: 'Razas de perros',
-                            data: [1, 1, 2, 3, 1, 2, 1, 2, 2, 1, 3, 3, 2, 1, 1, 2, 3, 1, 2, 2],
+                            label: nameDogs,
+                            data: countDogs,
                             backgroundColor: [
                                 'rgba(255, 99, 132, 0.2)',
                                 'rgba(54, 162, 235, 0.2)',
@@ -68,6 +101,11 @@ function search() {
                     }
                 });
             }
-
         });
 }
+
+
+function clearData () {
+    localStorage.clear();
+}
+
